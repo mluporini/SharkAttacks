c32c84675de60f21872ccb3f3f615d562df179e6
mluporini <120067057+mluporini@users.noreply.github.com>
2022-12-09 22:36:06 -0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -422,8 +422,8 @@ def update_charts(continent):
         
     return fig1, fig2
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
-    
 #if __name__ == '__main__':
-#    app.run_server(host= '0.0.0.0',port=8050)
+#    app.run_server(debug=True)
+    
+if __name__ == '__main__':
+    app.run_server(host= '0.0.0.0',port=8050)
